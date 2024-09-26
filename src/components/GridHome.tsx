9932fbd1541c199c7a1ecdc09d79846e66e94846
Erik Giuseppe Kato Bandeira <erik.setide@osasco.sp.gov.br>
2024-09-26 11:46:16 -0300

    atulizando o footer

--- src/components/GridHome.tsx
+++ src/components/GridHome.tsx
@@ -58,7 +58,8 @@ export default function RowAndColumnSpacing() {
                     </Item>
 
                 </Grid>
-                <Grid className="px-0 h-72  md:h-80 lg:h-96 xl:h-96 w-screen" size={6}>
+                <Grid className="px-0 h-72 mb-5  md:h-80 lg:h-96 xl:h-96 w-screen" size={6}>
+
                     <BackgroundImageComponent
 
                         image='url(/public/consorcio-casa-artigo.jpg)'
